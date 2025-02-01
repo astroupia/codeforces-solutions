6f810cf8ab4bd271e6d061813129f48663cfc8a1
astroupia <103548612+astroupia@users.noreply.github.com>
2025-02-01 21:59:05 +0300

    .\A_Sum.py

---
+++ A_Sum.py
@@ -0,0 +1,9 @@
+num =  int(input())
+
+for _ in range(num):
+    nums = list(map(int, input().split()))
+    nums.sort()
+    if (nums[0] + nums[1]) == nums[2]:
+        print("YES")
+    else:
+        print("NO")
