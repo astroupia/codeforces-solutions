18ef789f458779c74b4294da3a454ab52e178655
astroupia <103548612+astroupia@users.noreply.github.com>
2025-02-01 21:58:17 +0300

    .\C_Young_Physicist.py

---
+++ C_Young_Physicist.py
@@ -0,0 +1,15 @@
+num = int(input())
+
+sum_x, sum_y, sum_z = 0, 0, 0
+
+for _ in range(num):
+    x, y, z = map(int, input().split())
+
+    sum_x += x
+    sum_y += y
+    sum_z += z
+
+if sum_x == 0 and sum_y == 0 and sum_z == 0:
+    print("YES")
+else: 
+    print("NO")
