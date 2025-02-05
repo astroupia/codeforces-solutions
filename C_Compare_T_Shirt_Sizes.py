77b383ee4c0c43a67fa98010a89ecc5cd2dd37c2
astroupia <103548612+astroupia@users.noreply.github.com>
2025-02-05 23:44:52 +0300

    .\C_Compare_T_Shirt_Sizes.py

--- C_Compare_T_Shirt_Sizes.py
+++ C_Compare_T_Shirt_Sizes.py
@@ -3,36 +3,22 @@ num = int(input())
 for _ in range(num):
     a, b = map(str, input().split())
 
-    if a[-1] == "S":
-        if b[-1] == "S":
-            if len(a) > len(b):
-                print("<")
-            elif len(a) < len(b):
-                print(">")
-            else:
-                print("=")
-        elif b[-1] == "L":
-            print("<")
-        else:
-            print("<")
-    elif a[-1] == "L":
-        if b[-1] == "L":
-            if len(a) > len(b):
-                print(">")
-            elif len(a) < len(b):
-                print("<")
-            else:
-                print("=")
-        elif b[-1] == "S":
-            print(">")
-        else:
-            print(">")
-    elif a[-1] == "M":
-        if b[-1] == "S":
-            print(">")
-        elif b[-1] == "L":
-            print("<")
+    if a == b:
+        print("=")
+    elif a[-1] == b[-1]:
+        if a[-1] == "L":
+            print("<" if len(a) < len(b) else ">")
+        elif a[-1] == "S":
+            print(">" if len(a) < len(b) else "<")
         else:
             print("=")
-                
-        
+    else:
+        if a[-1] == "S":
+            print("<")
+        elif a[-1] == "L":
+            print(">")
+        else:
+            if b[-1] == "S":
+                print("<")
+            else:
+                print(">")
