a653ddfcc2482f3b06ce60e4231260e5a32b7a9e
astroupia <103548612+astroupia@users.noreply.github.com>
2025-02-08 21:14:03 +0300

    .\D_X_Sum.py

---
+++ D_X_Sum.py
@@ -0,0 +1,29 @@
+from collections import Counter
+
+t = int(input())
+
+for _ in range(t):
+    m, n = map(int, input().split())
+
+    chess = []
+
+    for i in range(m):
+        chess.append(list(map(int, input().split())))
+
+    count_main = Counter()
+    count_anti = Counter()
+
+    for i in range(m):
+        for j in range(n):
+            count_main[i + j] += chess[i][j]
+            count_anti[i-j] += chess[i][j]
+        
+    sum = 0
+    for i in range(m):
+        for j in range(n):
+            main_sum = count_main[i + j] - chess[i][j]
+            anti_sum = count_anti[i - j]
+            sum = max(sum, main_sum + anti_sum) 
+    
+
+    print(sum)
